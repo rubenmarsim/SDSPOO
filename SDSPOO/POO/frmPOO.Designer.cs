9c830e1cedc596e080aa3f3bff7a99e67943f192
Rubén Martínez <ruben.marsim10@gmail.com>
2018-11-17 09:57:33 +0100

    estructura form

--- SDSPOO/POO/frmPOO.Designer.cs
+++ SDSPOO/POO/frmPOO.Designer.cs
@@ -28,20 +28,270 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblImport = new System.Windows.Forms.Label();
+            this.lblIVA = new System.Windows.Forms.Label();
+            this.lblTipusIVA = new System.Windows.Forms.Label();
+            this.lblValorIVA1 = new System.Windows.Forms.Label();
+            this.lblValorIVA2 = new System.Windows.Forms.Label();
+            this.lblValorIVA3 = new System.Windows.Forms.Label();
+            this.txtBoxImport = new System.Windows.Forms.TextBox();
+            this.txtBoxIVA = new System.Windows.Forms.TextBox();
+            this.txtBoxTipusIVA = new System.Windows.Forms.TextBox();
+            this.txtBoxValorIVA1 = new System.Windows.Forms.TextBox();
+            this.txtBoxValorIVA2 = new System.Windows.Forms.TextBox();
+            this.txtBoxValorIVA3 = new System.Windows.Forms.TextBox();
+            this.txtBoxBeneficis = new System.Windows.Forms.TextBox();
+            this.txtBoxBeneficis2 = new System.Windows.Forms.TextBox();
+            this.btnCalculaIVA1 = new System.Windows.Forms.Button();
+            this.btnCalculaIVA2 = new System.Windows.Forms.Button();
+            this.btnCalculaIVA3 = new System.Windows.Forms.Button();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnSumaGutisa = new System.Windows.Forms.Button();
+            this.btnBeneficis = new System.Windows.Forms.Button();
+            this.btnBeneficis2 = new System.Windows.Forms.Button();
+            this.btnBenvinguda = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblImport
+            // 
+            this.lblImport.AutoSize = true;
+            this.lblImport.Location = new System.Drawing.Point(65, 71);
+            this.lblImport.Name = "lblImport";
+            this.lblImport.Size = new System.Drawing.Size(36, 13);
+            this.lblImport.TabIndex = 0;
+            this.lblImport.Text = "Import";
+            // 
+            // lblIVA
+            // 
+            this.lblIVA.AutoSize = true;
+            this.lblIVA.Location = new System.Drawing.Point(77, 112);
+            this.lblIVA.Name = "lblIVA";
+            this.lblIVA.Size = new System.Drawing.Size(24, 13);
+            this.lblIVA.TabIndex = 1;
+            this.lblIVA.Text = "IVA";
+            // 
+            // lblTipusIVA
+            // 
+            this.lblTipusIVA.AutoSize = true;
+            this.lblTipusIVA.Location = new System.Drawing.Point(48, 149);
+            this.lblTipusIVA.Name = "lblTipusIVA";
+            this.lblTipusIVA.Size = new System.Drawing.Size(53, 13);
+            this.lblTipusIVA.TabIndex = 2;
+            this.lblTipusIVA.Text = "Tipus IVA";
+            // 
+            // lblValorIVA1
+            // 
+            this.lblValorIVA1.AutoSize = true;
+            this.lblValorIVA1.Location = new System.Drawing.Point(447, 76);
+            this.lblValorIVA1.Name = "lblValorIVA1";
+            this.lblValorIVA1.Size = new System.Drawing.Size(67, 13);
+            this.lblValorIVA1.TabIndex = 3;
+            this.lblValorIVA1.Text = "Valor IVA #1";
+            // 
+            // lblValorIVA2
+            // 
+            this.lblValorIVA2.AutoSize = true;
+            this.lblValorIVA2.Location = new System.Drawing.Point(447, 115);
+            this.lblValorIVA2.Name = "lblValorIVA2";
+            this.lblValorIVA2.Size = new System.Drawing.Size(67, 13);
+            this.lblValorIVA2.TabIndex = 4;
+            this.lblValorIVA2.Text = "Valor IVA #2";
+            // 
+            // lblValorIVA3
+            // 
+            this.lblValorIVA3.AutoSize = true;
+            this.lblValorIVA3.Location = new System.Drawing.Point(447, 152);
+            this.lblValorIVA3.Name = "lblValorIVA3";
+            this.lblValorIVA3.Size = new System.Drawing.Size(67, 13);
+            this.lblValorIVA3.TabIndex = 5;
+            this.lblValorIVA3.Text = "Valor IVA #3";
+            // 
+            // txtBoxImport
+            // 
+            this.txtBoxImport.Location = new System.Drawing.Point(137, 71);
+            this.txtBoxImport.Name = "txtBoxImport";
+            this.txtBoxImport.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxImport.TabIndex = 6;
+            // 
+            // txtBoxIVA
+            // 
+            this.txtBoxIVA.Location = new System.Drawing.Point(137, 112);
+            this.txtBoxIVA.Name = "txtBoxIVA";
+            this.txtBoxIVA.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxIVA.TabIndex = 7;
+            // 
+            // txtBoxTipusIVA
+            // 
+            this.txtBoxTipusIVA.Location = new System.Drawing.Point(137, 149);
+            this.txtBoxTipusIVA.Name = "txtBoxTipusIVA";
+            this.txtBoxTipusIVA.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxTipusIVA.TabIndex = 8;
+            // 
+            // txtBoxValorIVA1
+            // 
+            this.txtBoxValorIVA1.Location = new System.Drawing.Point(544, 76);
+            this.txtBoxValorIVA1.Name = "txtBoxValorIVA1";
+            this.txtBoxValorIVA1.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxValorIVA1.TabIndex = 9;
+            // 
+            // txtBoxValorIVA2
+            // 
+            this.txtBoxValorIVA2.Location = new System.Drawing.Point(544, 115);
+            this.txtBoxValorIVA2.Name = "txtBoxValorIVA2";
+            this.txtBoxValorIVA2.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxValorIVA2.TabIndex = 10;
+            // 
+            // txtBoxValorIVA3
+            // 
+            this.txtBoxValorIVA3.Location = new System.Drawing.Point(544, 152);
+            this.txtBoxValorIVA3.Name = "txtBoxValorIVA3";
+            this.txtBoxValorIVA3.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxValorIVA3.TabIndex = 11;
+            // 
+            // txtBoxBeneficis
+            // 
+            this.txtBoxBeneficis.Location = new System.Drawing.Point(380, 266);
+            this.txtBoxBeneficis.Name = "txtBoxBeneficis";
+            this.txtBoxBeneficis.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxBeneficis.TabIndex = 12;
+            // 
+            // txtBoxBeneficis2
+            // 
+            this.txtBoxBeneficis2.Location = new System.Drawing.Point(380, 303);
+            this.txtBoxBeneficis2.Name = "txtBoxBeneficis2";
+            this.txtBoxBeneficis2.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxBeneficis2.TabIndex = 13;
+            // 
+            // btnCalculaIVA1
+            // 
+            this.btnCalculaIVA1.Location = new System.Drawing.Point(281, 71);
+            this.btnCalculaIVA1.Name = "btnCalculaIVA1";
+            this.btnCalculaIVA1.Size = new System.Drawing.Size(104, 23);
+            this.btnCalculaIVA1.TabIndex = 14;
+            this.btnCalculaIVA1.Text = "CalcularIVA #1";
+            this.btnCalculaIVA1.UseVisualStyleBackColor = true;
+            // 
+            // btnCalculaIVA2
+            // 
+            this.btnCalculaIVA2.Location = new System.Drawing.Point(281, 112);
+            this.btnCalculaIVA2.Name = "btnCalculaIVA2";
+            this.btnCalculaIVA2.Size = new System.Drawing.Size(104, 23);
+            this.btnCalculaIVA2.TabIndex = 15;
+            this.btnCalculaIVA2.Text = "CalcularIVA #2";
+            this.btnCalculaIVA2.UseVisualStyleBackColor = true;
+            // 
+            // btnCalculaIVA3
+            // 
+            this.btnCalculaIVA3.Location = new System.Drawing.Point(281, 149);
+            this.btnCalculaIVA3.Name = "btnCalculaIVA3";
+            this.btnCalculaIVA3.Size = new System.Drawing.Size(104, 23);
+            this.btnCalculaIVA3.TabIndex = 16;
+            this.btnCalculaIVA3.Text = "CalcularIVA #3";
+            this.btnCalculaIVA3.UseVisualStyleBackColor = true;
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Location = new System.Drawing.Point(46, 263);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(103, 23);
+            this.btnSuma.TabIndex = 17;
+            this.btnSuma.Text = "Suma";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            // 
+            // btnSumaGutisa
+            // 
+            this.btnSumaGutisa.Location = new System.Drawing.Point(46, 303);
+            this.btnSumaGutisa.Name = "btnSumaGutisa";
+            this.btnSumaGutisa.Size = new System.Drawing.Size(103, 23);
+            this.btnSumaGutisa.TabIndex = 18;
+            this.btnSumaGutisa.Text = "Suma Gutisa";
+            this.btnSumaGutisa.UseVisualStyleBackColor = true;
+            // 
+            // btnBeneficis
+            // 
+            this.btnBeneficis.Location = new System.Drawing.Point(224, 266);
+            this.btnBeneficis.Name = "btnBeneficis";
+            this.btnBeneficis.Size = new System.Drawing.Size(98, 23);
+            this.btnBeneficis.TabIndex = 19;
+            this.btnBeneficis.Text = "Beneficis";
+            this.btnBeneficis.UseVisualStyleBackColor = true;
+            // 
+            // btnBeneficis2
+            // 
+            this.btnBeneficis2.Location = new System.Drawing.Point(224, 303);
+            this.btnBeneficis2.Name = "btnBeneficis2";
+            this.btnBeneficis2.Size = new System.Drawing.Size(98, 23);
+            this.btnBeneficis2.TabIndex = 20;
+            this.btnBeneficis2.Text = "Beneficis #2";
+            this.btnBeneficis2.UseVisualStyleBackColor = true;
+            // 
+            // btnBenvinguda
+            // 
+            this.btnBenvinguda.Location = new System.Drawing.Point(578, 283);
+            this.btnBenvinguda.Name = "btnBenvinguda";
+            this.btnBenvinguda.Size = new System.Drawing.Size(93, 23);
+            this.btnBenvinguda.TabIndex = 21;
+            this.btnBenvinguda.Text = "Benvinguda";
+            this.btnBenvinguda.UseVisualStyleBackColor = true;
             // 
             // frmPOO
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBenvinguda);
+            this.Controls.Add(this.btnBeneficis2);
+            this.Controls.Add(this.btnBeneficis);
+            this.Controls.Add(this.btnSumaGutisa);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.btnCalculaIVA3);
+            this.Controls.Add(this.btnCalculaIVA2);
+            this.Controls.Add(this.btnCalculaIVA1);
+            this.Controls.Add(this.txtBoxBeneficis2);
+            this.Controls.Add(this.txtBoxBeneficis);
+            this.Controls.Add(this.txtBoxValorIVA3);
+            this.Controls.Add(this.txtBoxValorIVA2);
+            this.Controls.Add(this.txtBoxValorIVA1);
+            this.Controls.Add(this.txtBoxTipusIVA);
+            this.Controls.Add(this.txtBoxIVA);
+            this.Controls.Add(this.txtBoxImport);
+            this.Controls.Add(this.lblValorIVA3);
+            this.Controls.Add(this.lblValorIVA2);
+            this.Controls.Add(this.lblValorIVA1);
+            this.Controls.Add(this.lblTipusIVA);
+            this.Controls.Add(this.lblIVA);
+            this.Controls.Add(this.lblImport);
             this.Name = "frmPOO";
             this.Text = "frmPOO";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblImport;
+        private System.Windows.Forms.Label lblIVA;
+        private System.Windows.Forms.Label lblTipusIVA;
+        private System.Windows.Forms.Label lblValorIVA1;
+        private System.Windows.Forms.Label lblValorIVA2;
+        private System.Windows.Forms.Label lblValorIVA3;
+        private System.Windows.Forms.TextBox txtBoxImport;
+        private System.Windows.Forms.TextBox txtBoxIVA;
+        private System.Windows.Forms.TextBox txtBoxTipusIVA;
+        private System.Windows.Forms.TextBox txtBoxValorIVA1;
+        private System.Windows.Forms.TextBox txtBoxValorIVA2;
+        private System.Windows.Forms.TextBox txtBoxValorIVA3;
+        private System.Windows.Forms.TextBox txtBoxBeneficis;
+        private System.Windows.Forms.TextBox txtBoxBeneficis2;
+        private System.Windows.Forms.Button btnCalculaIVA1;
+        private System.Windows.Forms.Button btnCalculaIVA2;
+        private System.Windows.Forms.Button btnCalculaIVA3;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnSumaGutisa;
+        private System.Windows.Forms.Button btnBeneficis;
+        private System.Windows.Forms.Button btnBeneficis2;
+        private System.Windows.Forms.Button btnBenvinguda;
     }
 }
 
